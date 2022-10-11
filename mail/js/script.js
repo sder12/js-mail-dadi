889929f9bcb08c273ea83cc91c303be8b7df8c93
sder12 <sara.cetto@gmail.com>
2022-10-11 14:45:56 +0200

    Start email-exercise with comments

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,3 +7,12 @@ Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 */
 
 alert("Hello email!")
+
+//CREARE ARROW con un elenco di fake emails
+//es emails: pluto@gmail.com paperino@gmail.com topolino@gmail.com 
+// CHIEDERE ALL'UTENTE l'email 
+//SCORRERE la lista e verificare se email dell'utente sia nella arrow
+//SE l'email c'è nella lista 
+//  messaggio: email corretta, benvenuto
+//ALTRIMENTI
+//  messaggio: email errata, riprovare 
