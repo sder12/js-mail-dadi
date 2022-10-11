948f2a879423a0f59be2b3b574a77711269ee197
sder12 <sara.cetto@gmail.com>
2022-10-11 14:50:54 +0200

    Adding the array and prompt

--- mail/js/script.js
+++ mail/js/script.js
@@ -6,11 +6,15 @@ Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 */
 
-alert("Hello email!")
 
-//CREARE ARROW con un elenco di fake emails
-//es emails: pluto@gmail.com paperino@gmail.com topolino@gmail.com 
+//CREARE ARRAY con un elenco di fake emails
+//es emails: pluto@gmail.com paperino@gmail.com topolino@gmail.com
+const emailList = ["pluto@gmail.com", "paperino@gmail.com", "topolino@gmail.com", "topolina@gmail.com", "cipeciop@gmail.com", "pippo@gmail.com" ]; 
+console.log(emailList)
 // CHIEDERE ALL'UTENTE l'email 
+const userEmail = prompt("Qual'è la tua email?")
+console.log(userEmail)
+
 //SCORRERE la lista e verificare se email dell'utente sia nella arrow
 //SE l'email c'è nella lista 
 //  messaggio: email corretta, benvenuto
