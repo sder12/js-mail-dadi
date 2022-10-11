f86b73ffe99bb8d722fbcf73099daa5367a8aa6b
sder12 <sara.cetto@gmail.com>
2022-10-11 14:27:20 +0200

    Template + Initialize

---
+++ gioco-dei-dadi/js/script.js
@@ -0,0 +1,10 @@
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, 
+sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+----
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+*/
+
+alert("Hello!Gioco dei dadi")
---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+/*Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+----
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+*/
+
+alert("Hello email!")
