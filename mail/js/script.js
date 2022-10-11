3f5ed877b83f30a2efac21ab8f66c6cfc8cef2f4
sder12 <sara.cetto@gmail.com>
2022-10-11 14:59:31 +0200

    Finish email-exercise

--- mail/js/script.js
+++ mail/js/script.js
@@ -16,7 +16,24 @@ const userEmail = prompt("Qual'è la tua email?")
 console.log(userEmail)
 
 //SCORRERE la lista e verificare se email dell'utente sia nella arrow
+//variabile esterna al for-array
+let isFound = false;
+for(let i = 0; i < emailList.length; i++){
+    //creare variabile per ogni singolo elemento della lista 
+    const emailListItem = emailList[i];
+    console.log(emailListItem)
+    //creare SE email è nella lista è vero
+    if(emailListItem === userEmail){
+        isFound = true;
+    }
+}
+
+if (isFound){
+    document.writeln("email corretta, benvenuto")
+} else{
+    document.writeln("email errata, riprovare")
+}
 //SE l'email c'è nella lista 
 //  messaggio: email corretta, benvenuto
 //ALTRIMENTI
-//  messaggio: email errata, riprovare 
+//  messaggio: email errata, riprovare
